f9f65c9a09a9a159ddc9bfb39ae5ca0b3c882bdf
AngelFireLA <55795775+AngelFireLA@users.noreply.github.com>
2023-04-06 16:26:55 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,41 +3,28 @@ import io
 from flask_migrate import Migrate
 from werkzeug.utils import secure_filename
 import os
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, DateField, SelectField, FileField
+from wtforms.validators import DataRequired, Email
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from models import forms
 from flask_login import current_user, login_user, logout_user, login_required
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import Flask, flash, get_flashed_messages, redirect, render_template, request, url_for, make_response, session, \
-    send_file
-
-from models.forms import UploadForm
+from flask import Flask, flash, redirect, render_template, request, url_for, make_response, session, send_file, abort
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = "login"
-migrate = Migrate(app, db)
-
-
-class Homework(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), index=True, unique=True)
-    content = db.Column(db.String(2000))
-    due_date = db.Column(db.String(50))
-    teacher = db.Column(db.String(100))
-    subject = db.Column(db.String(100))
-    class_id = db.Column(db.String(100))
-    user_id = db.Column(db.String(200), nullable=True)
-    file_data = db.Column(db.LargeBinary, nullable=True)
-    file_name = db.Column(db.String(100), nullable=True)
+migrate = Migrate(app, db, render_as_batch=True)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=False)
+    status = db.Column(db.String(64), unique=False)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
@@ -51,6 +38,25 @@ class User(UserMixin, db.Model):
     def load_user(id):
         return User.query.get(int(id))
 
+class Homework(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), index=True, unique=True)
+    content = db.Column(db.String(2000))
+    due_date = db.Column(db.String(50))
+    teacher = db.Column(db.String(100))
+    subject = db.Column(db.String(100))
+    class_id = db.Column(db.String(100))
+    user_id = db.Column(db.String(200))
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), index=True, unique=True)
+    content = db.Column(db.String(2000))
+    user_id = db.Column(db.String(200))
+    due_date = db.Column(db.String(50))
+    file_data = db.Column(db.LargeBinary, nullable=True)
+    file_name = db.Column(db.String(100), nullable=True)
+
 with app.app_context():
     db.create_all()
 
@@ -60,10 +66,23 @@ def homework():
     homework_list = Homework.query.all()
     return render_template('homeworks.html', homework_list=homework_list)
 
+def change_user_status(email, status):
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        print(f"User with email '{email}' not found.")
+        return
+
+    user.status = status
+    db.session.commit()
+    print(f"User with email '{email}' status changed to '{status}'.")
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+
+@app.route('/upload_homework', methods=['GET', 'POST'])
+@login_required
 def upload():
+    if not current_user.status == "admin":
+        abort(403)
     form = UploadForm()
     if request.method == 'POST':
         title = request.form['title']
@@ -111,7 +130,7 @@ def upload():
         flash('Devoir envoyé correctement.', 'success')
         return redirect(url_for('homework'))
     else:
-        return render_template('upload.html', form=form)
+        return render_template('upload_hw.html', form=form)
 
 
 
@@ -119,7 +138,7 @@ def upload():
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
-    form = forms.RegistrationForm()
+    form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.name.data, email=form.email.data)
         user.set_password(form.password.data)
@@ -134,7 +153,7 @@ def register():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
-    form = forms.LoginForm()
+    form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email_or_username.data).first()
         if user is None or not user.check_password(form.password.data):
@@ -150,7 +169,7 @@ def login():
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
-    form = forms.UpdateProfileForm()
+    form = UpdateProfileForm()
     if form.validate_on_submit():
         if current_user.check_password(form.password.data):
             user = User.query.filter_by(id=current_user.id).first()
@@ -181,7 +200,6 @@ def upload_error():
 @app.route('/')
 @app.route('/home')
 def home():
-    print(current_user.username)
     return render_template('index.html')
 
 
@@ -195,5 +213,43 @@ def download_file(homework_id):
     response.headers['Content-Disposition'] = 'attachment; filename={}'.format(homework.file_name)
     return response
 
+class RegistrationForm(FlaskForm):
+    name = StringField('Prénom', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Mot de passe', validators=[DataRequired()])
+    submit = SubmitField("S'inscrire")
+
+
+
+class LoginForm(FlaskForm):
+    email_or_username = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Mot de passe', validators=[DataRequired()])
+    submit = SubmitField('Se connecter')
+
+
+class UpdateProfileForm(FlaskForm):
+    name = StringField('Prénom', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Entrer le mot de passe pour confirmer les changements.', validators=[DataRequired()])
+    submit = SubmitField('Sauvegarder les changements.')
+
+class UploadForm(FlaskForm):
+    classes = ["1 G-B"]
+    matieres = ["Français", "Histoire Géographie", "Espagnol", "Anglais", "Sport", "ES Physique", "ES Maths", "ES Svt"]
+    profs = ["MARCHINI", "MALPELLI", "CABARCOS", "MUSSI", "RIGAUX", "BRUNINI", "AVENOSO", "CASTELLO", "CHAPUIS", "MURACCIOLES", "ROMEO"]
+    title = StringField('Titre', validators=[DataRequired()])
+    content = StringField('Contenu', validators=[DataRequired()])
+    due_date = DateField('Date Limite', format='%d-%m-%Y')
+    teacher = SelectField('Professeur', choices=profs)
+    subject = SelectField('Matière', choices=matieres)
+    classe = SelectField('Classe', choices=classes)
+    submit = SubmitField('Envoyer')
+
+class AnswerForm(FlaskForm):
+    title = StringField('Titre', validators=[DataRequired()])
+    content = StringField('Contenu', validators=[DataRequired()])
+    file = FileField('file', validators=[])
+    submit = SubmitField('Envoyer')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='127.0.0.1', port=3000)
